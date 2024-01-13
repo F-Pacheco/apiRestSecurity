9d8734fd7602d3c28b5c38c6807706667b8f3ab7
FedePach <fm.pachec@gmail.com>
2024-01-13 03:33:25 -0300

    Se agrega otro usuario fake hardcodeado para comenzar la implementacion de autorizaciones

--- src/main/java/com/apiRestSecurity/demo/service/UserService.java
+++ src/main/java/com/apiRestSecurity/demo/service/UserService.java
@@ -9,15 +9,28 @@ import java.util.Optional;
 public class UserService {
 
     private static final String EXISTING_EMAIL= "fedekpo";
+    private static final String ANOTHER_EMAIL= "marikpa";
     public Optional<UserEntity> findByEmail(String email){
-        if(!EXISTING_EMAIL.equalsIgnoreCase(email)) return Optional.empty();
-        var user = new UserEntity();
-        user.setId(1L);
-        user.setEmail(EXISTING_EMAIL);
-        user.setPassword("$2a$12$UgeWgjdxZqI9Yzhdjly2zObuf6PLvvXz9XiPgG2XMcgYOREoXPfIi");//12345
-        user.setRole("ROLE_ADMIN");
-        user.setExtraInfo("Re crack el admin");
-        return Optional.of(user);
+        if(EXISTING_EMAIL.equalsIgnoreCase(email)){
+            var user = new UserEntity();
+            user.setId(1L);
+            user.setEmail(EXISTING_EMAIL);
+            user.setPassword("$2a$12$UgeWgjdxZqI9Yzhdjly2zObuf6PLvvXz9XiPgG2XMcgYOREoXPfIi");//12345
+            user.setRole("ROLE_ADMIN");
+            user.setExtraInfo("Re crack el admin");
+            return Optional.of(user);
+        }else if (ANOTHER_EMAIL.equalsIgnoreCase(email)){
+            var user = new UserEntity();
+            user.setId(99L);
+            user.setEmail(ANOTHER_EMAIL);
+            user.setPassword("$2a$12$UgeWgjdxZqI9Yzhdjly2zObuf6PLvvXz9XiPgG2XMcgYOREoXPfIi");//12345
+            user.setRole("ROLE_USER");
+            user.setExtraInfo("Re crack el user");
+            return Optional.of(user);
+        }
+        return Optional.empty();
+
+
 
  }
 }
