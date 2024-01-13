2101d71be551ed84f25e819e279cc22d17053e28
FedePach <fm.pachec@gmail.com>
2024-01-13 03:28:47 -0300

    Se agrega el servicio AuthService para que se encargue del login, dejando limpio el controlador

--- src/main/java/com/apiRestSecurity/demo/controllers/AuthController.java
+++ src/main/java/com/apiRestSecurity/demo/controllers/AuthController.java
@@ -4,6 +4,7 @@ import com.apiRestSecurity.demo.model.LoginRequest;
 import com.apiRestSecurity.demo.model.LoginResponse;
 import com.apiRestSecurity.demo.security.JwtIssuer;
 import com.apiRestSecurity.demo.security.UserPrincipal;
+import com.apiRestSecurity.demo.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -22,22 +23,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final JwtIssuer jwtIssuer;
-    private final AuthenticationManager authenticationManager;
+    private final AuthService authService;
     @PostMapping("/auth/login")
     public LoginResponse login(@RequestBody @Validated LoginRequest request){
-        var authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword())
-        );
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        var principal = (UserPrincipal) authentication.getPrincipal();
-        var roles = principal.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .toList();
-        var token = jwtIssuer.issue(principal.getUserId(), principal.getEmail(), roles);
-        return LoginResponse.builder()
-                .accesToken(token)
-                .build();
+        return authService.attemptLoggin(request.getEmail(),request.getPassword());
 
 }
 
---
+++ src/main/java/com/apiRestSecurity/demo/service/AuthService.java
@@ -0,0 +1,34 @@
+package com.apiRestSecurity.demo.service;
+
+import com.apiRestSecurity.demo.model.LoginResponse;
+import com.apiRestSecurity.demo.security.JwtIssuer;
+import com.apiRestSecurity.demo.security.UserPrincipal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final JwtIssuer jwtIssuer;
+    private final AuthenticationManager authenticationManager;
+
+    public LoginResponse attemptLoggin(String email, String password){
+        var authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email,password)
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        var principal = (UserPrincipal) authentication.getPrincipal();
+        var roles = principal.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+        var token = jwtIssuer.issue(principal.getUserId(), principal.getEmail(), roles);
+        return LoginResponse.builder()
+                .accesToken(token)
+                .build();
+    }
+
+}
